c0843d7c1a5dd1a5b68e966a13bd05179433c8f6
pagecge <pagecge@localhost>
2019-01-24 19:30:39 +0800

    在eclipse上修改的分支2 hell2——branch

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello{
 	public static void main(String[] args){
 		System.out.print("Hello World !!!");
+		System.out.println("wo shi hell2_branch");
 	}
 	public static void print1() {
 		System.out.println("1");
